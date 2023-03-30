727ce762171728cd83c88f2212746c6573cf8fef
akashdeep931 <akashdeepsingh931@gmail.com>
2023-03-30 19:24:29 +0100

    organise the promises to work correctly

--- controllers/delete.controllers.js
+++ controllers/delete.controllers.js
@@ -6,7 +6,7 @@ const {
 exports.deleteComments = (req, res, next) => {
   const { comment_id } = req.params;
 
-  Promise.all([deleteComment(comment_id), checkCommentExists(comment_id)])
+  Promise.all([checkCommentExists(comment_id), deleteComment(comment_id)])
     .then((data) => {
       res.status(204).send(data[0]);
     })
